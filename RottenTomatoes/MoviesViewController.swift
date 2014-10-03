717e99d0abdc98e6bcaf1a13b47807b0812c9ec4
Peter Bohnert <pbohnert@gmail.com>
2014-10-03 09:46:17 -0700

    finally got model for Movie Details working, including images.  Seem to have lost my header however...

--- RottenTomatoes/MoviesViewController.swift
+++ RottenTomatoes/MoviesViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-    var movies: [NSDictionary] = []
+    var movieDetails =  [MovieDetails]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +24,10 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         var request = NSURLRequest(URL: NSURL(string: url))
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
             var object = NSJSONSerialization.JSONObjectWithData(data, options:  nil, error: nil) as NSDictionary
-            
-            self.movies = object["movies"] as [NSDictionary]
+            self.movieDetails = MovieDetails.moviesWithJSON(object["movies"] as NSArray)
             self.tableView.backgroundColor = UIColor.blackColor()
             self.tableView.reloadData()
+
         }
         // Do any additional setup after loading the view.
     }
@@ -38,29 +38,26 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return self.movieDetails.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         var cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as MovieCell
         
-        var movie = movies[indexPath.row]
+        let movie = self.movieDetails[indexPath.row] as MovieDetails
         
-        cell.titleLabel.text = movie["title"] as? String
-        cell.synopsisLabel.text = movie["synopsis"] as? String
+        cell.titleLabel.text = movie.title
+        cell.synopsisLabel.text = movie.synopsis
         cell.backgroundColor = UIColor.blackColor()
         cell.titleLabel.textColor = UIColor.whiteColor()
         cell.synopsisLabel.textColor = UIColor.whiteColor()
         //cell.selectionStyle = UITableViewCellSelectionStyle.Gray
         //cell.selectedBackgroundView.backgroundColor = UIColor(white: 0.8, alpha: 0.4)
         
-        var posters = movie["posters"] as NSDictionary
-        var posterUrl = posters["thumbnail"] as String
+        var posterUrl = movie.thumbnailImageURL as String
         
         cell.posterView.setImageWithURL(NSURL(string: posterUrl))
-        
-        //cell.textLabel!.text = "Hello, I'm at row: \(indexPath.row), section: \(indexPath.section)"
         
         return cell
     }
